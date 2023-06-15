b12d96061e68bc11cf3cded723dbb1aa3a54b7c6
iwatchme <yangxiao82739671@gmail.com>
2023-06-15 21:57:04 +0800

    add editviewmodel

---
+++ app/src/main/java/com/iwatchme/jetpackstarter/pictureEditor/DrawingArea.kt
@@ -0,0 +1,47 @@
+package com.iwatchme.jetpackstarter.pictureEditor
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun DrawingArea(
+    modifier: Modifier,
+    selectTool: EditorTool,
+    drawObjects: List<EditorObject>,
+    currentPath: EditorObject,
+    handleEvent: (event: EditorEvent) -> Unit
+) {
+    Canvas(modifier = modifier.pointerInteropFilter { event ->
+        if (selectTool is EditorTool.BrushTool) {
+            handleEvent(EditorEvent.BrushEvent(event))
+            true
+        } else {
+            false
+        }
+
+
+    }) {
+        (drawObjects + currentPath)
+            .filterIsInstance<EditorObject.BrushPath>().forEach { drawingObject ->
+                drawPath(
+                    path = drawingObject.path.value,
+                    color = drawingObject.brushConfiguration.color,
+                    style = Stroke(
+                        drawingObject
+                            .brushConfiguration.thickness,
+                        cap = StrokeCap.Round,
+                        join = StrokeJoin.Round
+                    )
+                )
+            }
+    }
+
+}
