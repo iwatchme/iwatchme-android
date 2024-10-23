4c8223142fe2d0a24cafe21abc4361d0dcebef8a
yangxiao02 <yangxiao02@bilibili.com>
2024-10-23 13:22:29 +0800

    feat: add theme, icon and color

--- cryptotrack/build.gradle.kts
+++ cryptotrack/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.junit)
     androidTestImplementation(libs.androidx.test.espresso)
